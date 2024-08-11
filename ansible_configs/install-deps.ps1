5131ba013797e98f9e1c106aeef4462ad75ec8b3
MrAle98 <ale18109800@gmail.com>
2024-08-11 23:44:58 +0200

    add windows local ansible playbook

--- ansible_configs/install-deps.ps1
+++ ansible_configs/install-deps.ps1
@@ -6,6 +6,8 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 add-content -Path .\triplets\x64-windows-static.cmake -Value 'set(VCPKG_BUILD_TYPE release)'
 .\vcpkg.exe integrate install
-.\vcpkg.exe install protobuf:x64-windows-static cpr:x64-windows-static gzip-hpp:x64-windows-static libsodium:x64-windows-static stduuid:x64-windows-static botan:x64-windows-static
+cd ..
+.\vcpkg\vcpkg.exe x-update-baseline --add-initial-baseline
+.\vcpkg\vcpkg.exe install --triplet=x64-windows-static --host-triplet=x64-windows-static --allow-unsupported
 $cmake_path=gci -force -Recurse -Include "cmake.exe" | select -ExpandProperty DirectoryName
 setx PATH "$cmake_path;$env:path" -m
