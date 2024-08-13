6a719060d824947d7cd0dc52c49aa0082ac8780e
MrAle98 <ale18109800@gmail.com>
2024-08-13 20:04:47 +0200

    updated MemoryModulePP and build-server.sh

--- build-servers.sh
+++ build-servers.sh
@@ -4,10 +4,16 @@ cd sliver
 git checkout refactor/teamserver-interaction
 ./go-assets.sh
 make linux
+rm -rf ~/sliver-builds
+mkdir ~/sliver-builds
 cp sliver-server ../ansible_configs/sliver
 cp sliver-client ../ansible_configs/sliver
+cp sliver-server ~/sliver-builds/
+cp sliver-client ~/sliver-builds/
 git checkout cppimplant
 ./go-assets.sh
 make windows
 cp sliver-server.exe ../ansible_configs/sliver
 cp sliver-client.exe ../ansible_configs/sliver
+cp sliver-server.exe ~/sliver-builds/
+cp sliver-client.exe ~/sliver-builds/
